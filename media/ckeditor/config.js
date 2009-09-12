db6f2f45f7ac91c7baf12a0e9ee15ad30c73ff71
mickek <mklujszo@gmail.com>
2009-09-12 08:32:42 +0200

    integrated ckeditor

---
+++ media/ckeditor/config.js
@@ -0,0 +1,17 @@
+﻿/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	config.language = 'pl';
+	config.toolbar = 'Basic';
+
+	config.toolbar_Basic =
+		[
+		    ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink'],
+		    ['-','Outdent','Indent','Blockquote'],
+		    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock']
+		];
+};
