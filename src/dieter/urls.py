69390fa5cee37fa8044992f64dce49075dbbb1a4
mickek <mklujszo@gmail.com>
2009-09-18 11:07:13 +0200

    #75 removed admin url

--- src/dieter/urls.py
+++ src/dieter/urls.py
@@ -19,11 +19,10 @@ urlpatterns = patterns('',
     url(r'^admin/(.*)', admin.site.root),
 )
 
-#in local mode, media files are served this way
 if settings.MEDIA_ROOT != '' and \
-   settings.MEDIA_URL.startswith('/') and \
-   settings.ADMIN_MEDIA_PREFIX.startswith('/'):
-    urlpatterns += patterns('',
-        (r'^' + settings.MEDIA_URL[1:] + r'(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-        (r'^' + settings.ADMIN_MEDIA_PREFIX[1:] + r'(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.abspath(os.path.join(settings.DJANGO_PATH, 'contrib/admin/media/'))}),
-    )
+    settings.MEDIA_URL.startswith('/') and \
+    settings.ADMIN_MEDIA_PREFIX.startswith('/'):
+        urlpatterns += patterns('',
+            (r'^' + settings.MEDIA_URL[1:] + r'(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+            (r'^' + settings.ADMIN_MEDIA_PREFIX[1:] + r'(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.abspath(os.path.join(settings.DJANGO_PATH, 'contrib/admin/media/'))}),
+        )
