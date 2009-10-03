743c40ea07efed7924a2724413ddd4613305c4ab
mickek <mklujszo@gmail.com>
2009-10-03 22:36:33 +0200

    #80 upewnic sie ze transactional middleware jest zainstalowany

--- src/dieter/settings/base.py
+++ src/dieter/settings/base.py
@@ -20,6 +20,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
 
 )
 
